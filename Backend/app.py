4d13801c028c52aa40a105fcba61abe461a1a356
Mahim <mahim.g03@gmail.com>
2024-01-16 00:40:21 +0530

    CORS error

--- Backend/app.py
+++ Backend/app.py
@@ -9,7 +9,7 @@ import yfinance as yf
 # instantiate flask
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://13.201.92.248"}})
+CORS(app, resources={r"/*": {"origins": "https://dportfolio-analyst.netlify.app/"}})
 
 
 @app.route('/')
