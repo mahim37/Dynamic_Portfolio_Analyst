7707a62074ae18b5259874fca6e10668a19832ba
Mahim <mahim.g03@gmail.com>
2024-01-16 00:46:09 +0530

    cors

--- Backend/app.py
+++ Backend/app.py
@@ -9,7 +9,8 @@ import yfinance as yf
 # instantiate flask
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://dportfolio-analyst.netlify.app/"}})
+# CORS(app, resources={r"/*": {"origins": "https://dportfolio-analyst.netlify.app/"}})
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 
 @app.route('/')
